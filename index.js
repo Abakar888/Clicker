6d8a0bb1f5c41898683c1876692c28f291cbcf20
Абакар <akap302@gmail.com>
2021-02-18 18:27:53 +0300

    начал делать кликер

---
+++ index.js
@@ -0,0 +1,13 @@
+let counter = document.querySelector('.wrapper__counter');
+let plus = document.querySelector('.wrapper__plus');
+let minus = document.querySelector('.wrapper__minus');
+let count = 0;
+plus.addEventListener('click', () => {
+    console.log(counter.textContent = count++);
+
+});
+
+minus.addEventListener('click', () => {
+    console.log(counter.textContent = count--);
+
+})
