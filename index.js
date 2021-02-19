08e55c5fa0f986b951c5178cc5254c1e271f6fa7
Абакар <akap302@gmail.com>
2021-02-19 18:39:49 +0300

    r

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ let plus = document.querySelector('.wrapper__plus');
 let minus = document.querySelector('.wrapper__minus');
 let count = 0;
 plus.addEventListener('click', () => {
-    console.log(counter.textContent = ++count);
+    counter.textContent = ++count;
 
 
 });
 
 minus.addEventListener('click', () => {
-    console.log(counter.textContent = --count);
+    counter.textContent = --count;
     if (count <= 0) {
         count = 0;
         counter.textContent = 0;
     }
-
-
 });
